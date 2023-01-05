fa9f15a146c8c67a9a30a04ae0fb45ab2578308b
Sergio Ruiz <serit250@gmail.com>
2023-01-05 11:01:12 -0600

    Fin sección 11 - version 4.0.0

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const {Router}=require('express')
 const {body,param}=require('express-validator')
 
-const {login}=require('../controllers/auth')
+const {login, googleSignIn}=require('../controllers/auth')
 const { validarCampos } = require('../middlewares/validar-campos')
 
 
@@ -13,6 +13,10 @@ router.post('/login',[
     validarCampos
 ],login)
 
+router.post('/google',[
+    body('id_token','id_token de google es necesario').not().isEmpty(),
+    validarCampos
+],googleSignIn)
 
 
 module.exports=router
